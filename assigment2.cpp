c7079f2a47ea57b30a78e5918e1c21542558ada3
Jawadahmad9938 <116241553+Jawadahmad9938@users.noreply.github.com>
2022-10-23 07:59:56 -0700

    Add files via upload

---
+++ assigment2.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int x , y;
+    char choose;
+    while(true)
+    {
+    cout<<"Enter First Integar "<<endl;
+    cin>>x;
+    cout<<"Enter Second Integar "<<endl;
+    cin>>y;
+    cout<<"Choose Your Operation (+ - * / %)";
+    cin>>choose;
+
+
+   if(choose=='+')
+   cout<<"Addition of "<<x<<" + "<<y<<" = "<<x+y<<endl;
+   else if(choose=='-')
+   cout<<"Subtraction of "<<x<<" - "<<y<<" = "<<x-y<<endl;
+   else if(choose=='*')
+   cout<<"Multiplication of "<<x<<" * "<<y<<" = "<<x*y<<endl;
+   else if(choose=='/')
+   cout<<"Division of "<<x<<" / "<<y<<" = "<<x/y<<endl;
+   else if(choose=='%')
+   cout<<"Modulus of "<<x<<" % "<<y<<" = "<<x%y<<endl;
+   else
+   break;
+
+}
+   cout<<"Invalid Input. The Loop is Terminated. "<<endl;
+   cout<<"----------------------------------------"<<endl;
+   cout<<"Thank You for using This Service."<<endl;
+}
